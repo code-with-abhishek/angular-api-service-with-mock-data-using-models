9dfb72be8d48e3dfd44d7db5d933f431ace603ff
Abhishek Singh <22190753+code-with-abhishek@users.noreply.github.com>
2020-01-04 15:47:19 +0530

    Update product.ts

--- src/app/home/product.ts
+++ src/app/home/product.ts
@@ -1,10 +1,7 @@
 export class Product {
     id: number;
     name: string;
-    cost: number;
     quantity: number;
-    locationId: number;
-    familyId: number;
     constructor() { 
     }
  } 
